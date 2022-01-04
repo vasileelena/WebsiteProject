92786f43e132d71e8630d079bb2d7e5a4505b17a
Elena <elle.alexxandra@gmail.com>
2022-01-04 18:14:47 +0200

    etapa 3 si jumate 4

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const express = require('express');
+const fs = require('fs');
+const sharp = require('sharp');
 
 app = express();
 
 app.set("view engine", "ejs");
 app.use("/resources", express.static(__dirname + "/resources"));
 
-app.get("/", function(req, res){
+function createImages() {
+  var buf = fs.readFileSync(__dirname + "/resources/json/galerie.json").toString("utf-8");
+  obImagini = JSON.parse(buf);
+
+  let dim_mic = 150;
+  let dim_mediu = 250;
+
+  for (let imag of obImagini.imagini){
+    [nume_imag, extensie] = imag.fisier.split(".");
+    console.log(nume_imag, extensie);
+    //cream o proprietate noua pt fiecare ob imagine din vector
+    imag.mic = `${obImagini.cale_galerie}/mic/${nume_imag}-${dim_mic}.webp`;
+    imag.mediu = `${obImagini.cale_galerie}/mediu/${nume_imag}-${dim_mediu}.webp`;
+    imag.mare = `${obImagini.cale_galerie}/${nume_imag}.png`;
+    //facem resize pt fiecare
+    if(!fs.existsSync(imag.mic)){
+      sharp(__dirname + "/" + imag.mare).resize(dim_mic).toFile(__dirname + "/" + imag.mic);
+    }
+    if(!fs.existsSync(imag.mediu)){
+      sharp(__dirname + "/" + imag.mare).resize(dim_mediu).toFile(__dirname + "/" + imag.mediu);
+    }
+  }
+}
+
+createImages();
+
+app.get(["/", "/index"], function(req, res){
   console.log(req.url);
-  res.render("pagini/index.ejs"); //calea e relativa la folderul views
+  console.log(req.ip);
+
+  // -------galerie
+
+  res.render("pagini/index.ejs", {ip:req.ip, imagini:obImagini.imagini, cale:obImagini.cale_galerie}); //calea e relativa la folderul views
 });
 
 app.get("/ceva", function(req, res){
